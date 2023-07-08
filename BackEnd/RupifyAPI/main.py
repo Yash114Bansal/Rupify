7fa29945ee5d57d7f45d7cac59cd56efce51a12b
yash114bansal <yash114bansal@gmail.com>
2023-07-08 11:18:34 +0530

    Added Encryption

---
+++ BackEnd/RupifyAPI/main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse, JSONResponse
+from pydantic import BaseModel
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad
+import json
+import base64
+import re
+
+key_hex = '12345678901234567890123456789ABC'
+key = bytes.fromhex(key_hex)
+
+
+def encrypt_message(key, message):
+    iv = get_random_bytes(AES.block_size)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    padded_message = pad(message, AES.block_size)
+    ciphertext = cipher.encrypt(padded_message)
+    return base64.b64encode(iv + ciphertext).decode('utf-8')
+
+
+def decrypt_message(key, ciphertext_b64):
+    ciphertext = base64.b64decode(ciphertext_b64)
+    iv = ciphertext[:AES.block_size]
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    plaintext = cipher.decrypt(ciphertext[AES.block_size:])
+    return plaintext.decode('utf-8')
+
+
+class cash(BaseModel):
+    aadhar: str
+    note: str
+    purpose: int
+
+
+class money_get(BaseModel):
+    aadhar: str
+
+
+class shopkeeper(BaseModel):
+    note_code: str
+    shopkeeper_aadhar: str
+
+
+app = FastAPI()
