dde670d0e0057619ff1f2002f1338b15a2d7612b
Paras <vivekupadhayay18@gmail.com>
2023-07-08 11:59:23 +0530

    added a wallet page and notes present to the user

---
+++ FrontEnd/rupify/lib/Pages/Home/Wallet/wallet.dart
@@ -0,0 +1,261 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'available_notes.dart';
+import 'package:rupify/services/balance_statements.dart';
+
+class WalletScreen extends StatefulWidget {
+  final Map<String, int> Note_Data;
+  WalletScreen({required this.Note_Data});
+  @override
+  _WalletScreenState createState() => _WalletScreenState();
+}
+
+class _WalletScreenState extends State<WalletScreen> {
+  double _balance = 0;
+
+  List<Statement> transactions = [];
+
+  @override
+  void initState() {
+    super.initState();
+    for (var value in widget.Note_Data.values) {
+      _balance += value;
+      DateTime now = DateTime.now();
+      DateTime date = DateTime(now.year, now.month, now.day, now.hour, now.minute, now.second);
+      transactions.add(Statement(name: "Name", amount: '$value', date: '$date'));
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'Wallet',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        backgroundColor: Colors.white,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Card(
+            margin: const EdgeInsets.all(30),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(50.0),
+            ),
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              height: MediaQuery.of(context).size.height * 0.25,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50.0),
+                gradient: const LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [Color(0xFF6078EA), Color(0xFF17EAD9)],
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Balance',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    '₹$_balance',
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                    ),
+                  ),
+                  const Spacer(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const Spacer(),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => NotesScreen(Note_Data: widget.Note_Data)),
+                          );
+
+                        },
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: const Color(0xFF6078EA),
+                          backgroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                        ),
+                        child: const Text(
+                          'Available Notes',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF6078EA),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+
+          const SizedBox(height: 20),
+
+          Expanded(
+            child: Container(
+              color: Colors.white,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Text(
+                      'History',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      physics: const BouncingScrollPhysics(),
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      itemCount: transactions.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Card(
+                            margin: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+                            child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    '${index + 1}.',
+                                    style: const TextStyle(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  const SizedBox(width: 10),
+                                  Icon(
+                                    int.parse(transactions[index].amount) <= 0
+                                        ? Icons.arrow_circle_down_outlined
+                                        : Icons.arrow_circle_up_outlined,
+                                    color: int.parse(transactions[index].amount) >= 0
+                                        ? Colors.green
+                                        : Colors.red,
+                                  ),
+                                  const SizedBox(width: 10),
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Payment from ${transactions[index].name}',
+                                        style: TextStyle(
+                                          color: int.parse(transactions[index].amount) >= 0
+                                              ? Colors.green
+                                              : Colors.red,
+                                        ),
+                                      ),
+                                      const SizedBox(height: 2),
+                                      Text(
+                                        transactions[index].date,
+                                        style: TextStyle(
+                                          color: int.parse(transactions[index].amount) >= 0
+                                              ? Colors.green
+                                              : Colors.red,
+                                          fontSize: 10,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  const Spacer(),
+                                  Text(
+                                    '${transactions[index].amount}₹',
+                                    style: TextStyle(
+                                      color: int.parse(transactions[index].amount) >= 0
+                                          ? Colors.green
+                                          : Colors.red,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  if (transactions.isEmpty)
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(100, 0, 50, 100),
+                      child: Column(
+                        children: [
+                          Container(
+                            height: 100,
+                            width: 100,
+                            child: Center(
+                              child: Image.asset('assets/img/duck.png'),
+                            ),
+                          ),
+                          const Text(
+                            'Nothig to show here!!',
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.black54,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(30),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                elevation: 5,
+              ),
+              onPressed: () {},
+              child:const  Text(
+                'Make Payment',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+
+        ],
+      ),
+    );
+  }
+}
