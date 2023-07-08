730969e6639422010b65a19c0b64d7886e4fafd1
Paras <vivekupadhayay18@gmail.com>
2023-07-08 09:57:45 +0530

    added routes

---
+++ FrontEnd/rupify/lib/Pages/Home/dashboard.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class dashboard extends StatelessWidget {
+  const dashboard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('This will be our Dashboard'),
+    );
+  }
+}
---
+++ FrontEnd/rupify/lib/Pages/Login/login.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class login extends StatelessWidget {
+  const login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('This will be our login page'),
+    );
+  }
+}
