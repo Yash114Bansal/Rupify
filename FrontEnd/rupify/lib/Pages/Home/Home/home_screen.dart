e29bbfa5ebffc0b02baed8972dc35c3e315ebf62
Paras <vivekupadhayay18@gmail.com>
2023-08-14 16:08:33 +0530

    make a seperate widget for the extra widget

--- FrontEnd/rupify/lib/Pages/Home/Home/home_screen.dart
+++ FrontEnd/rupify/lib/Pages/Home/Home/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:rupify/Src/requirements.dart';
-import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class HomeScreen extends StatefulWidget {
   final UserModelPrimary user;
@@ -11,31 +10,10 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
 
-  RefreshController _refreshController =
-  RefreshController(initialRefresh: false);
-
-  void _onRefresh() async{
-    await Functions().fetchData(context, widget.user);
-    setState(() {
-
-    });
-    _refreshController.refreshCompleted();
-  }
-
-  void _onLoading() async{
-    await Functions().fetchData(context, widget.user);
-    setState(() {
-
-    });
-    _refreshController.loadComplete();
-  }
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
-
-    List<MapEntry<String, String>> entries = dashBoardIcons.entries.toList();
-
     return Container(
       color: const Color(0xFF172A48),
       child: Scaffold(
@@ -55,77 +33,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 )
             ),
 
-            Stack(
-              children: [
-                DraggableScrollableSheet(
-                  initialChildSize: 0.78,
-                  minChildSize: 0.78,
-                  maxChildSize: 1,
-                  builder: (BuildContext context, ScrollController scrollController) {
-                    return Stack(
-                      children: [
-                        ScrollConfiguration(
-                          behavior: const ScrollBehavior().copyWith(overscroll: false),
-                            child: SmartRefresher(
-                              enablePullDown: true,
-                              enablePullUp:  true,
-                              header: const WaterDropHeader(waterDropColor: Colors.white,complete: Text('Fetched Notes Successfully',style: TextStyle(color: Colors.white,fontSize: 20))),
-                              onRefresh: _onRefresh,
-                              onLoading: _onLoading,
-                              controller: _refreshController,
-                              child: ListView(
-                                controller: scrollController,
-                                physics: const ClampingScrollPhysics(),
-                                children: [
-                                  SizedBox(
-                                    height: height,
-                                    width: width,
-                                    child: Stack(
-                                      children: [
-                                        Positioned(
-                                          top:45,
-                                          left: 0,
-                                          right: 0,
-                                          child: ClipRRect(
-                                            borderRadius: const BorderRadius.only(topLeft: Radius.circular(30),topRight: Radius.circular(30)),
-                                            child: Container(
-                                              height: height,
-                                              width: width,
-                                              color: Colors.white,
-                                              child: SizedBox(
-                                                width: width,
-                                                child: Column(
-                                                  mainAxisSize: MainAxisSize.max,
-                                                  children: [
-                                                    const SizedBox(height: 65),
-                                                    paymentList(context, entries),
-                                                    people(context),
-                                                  ],
-                                                ),
-                                              ),
-                                            ),
-                                          ),
-                                        ),
-                                        Positioned(
-                                            top: 0,
-                                          left: 0,
-                                          right: 0,
-                                            child: paymentCard(context, widget.user),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                        ),
-
-                      ],
-                    );
-                  },
-                ),
-              ],
-            ),
+            paymentAndPeople(context, widget.user),
           ],
         ),
       ),
